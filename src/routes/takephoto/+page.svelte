ac929c8b6cfc805f4606f1c08bf05fa1db0d5458
Coala_바지회장 <talksis@nate.com>
2023-04-07 00:10:06 +0900

    일단 올려보자

--- src/routes/takephoto/+page.svelte
+++ src/routes/takephoto/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import PocketBase from 'pocketbase';
+
 	let video: HTMLVideoElement | undefined;
 	let canvas: HTMLCanvasElement;
 	let taking = false;
+	const pb = new PocketBase('http://127.0.0.1:8090');
+
 	onMount(async () => {
 		const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 		video = document.querySelector('video') ?? undefined;
 		if (video) video.srcObject = stream;
 	});
 
-	function takePhoto() {
+	async function takePhoto() {
 		if (taking) return;
 
 		taking = true;
@@ -18,18 +22,23 @@
 			context.fillStyle = '#AAA';
 			context.fillRect(0, 0, 640, 480);
 		}
-		setTimeout(() => {
+		setTimeout(async () => {
 			const context = canvas.getContext('2d');
 			if (context && video) {
 				context.drawImage(video, 0, 0, 640, 480);
 				taking = false;
 				const data = canvas.toDataURL('image/png');
-				canvas.toBlob((blob) => {
+				canvas.toBlob(async (blob) => {
 					console.log(blob);
 					const formData = new FormData();
 					if (blob) {
-						formData.append('file', blob, '어쩌구저쩌구.png');
-						console.log(formData.get('file'));
+						formData.append('name', '박성준');
+						formData.append('grade', '5');
+						formData.append('classNum', '5');
+						formData.append('field', blob);
+						const result = await pb.collection('photo').create(formData);
+						console.log(result);
+						// console.log(formData.get('file'));
 					}
 					return blob;
 				});
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import PocketBase from 'pocketbase';
 
-	// async function uploadTest() {
-	// 	const pb = new PocketBase('http://127.0.0.1:8090');
-	// 	const data = { name: '박성준', grade: 5, class: 5 };
-	// 	const record = await pb.collection('photo').create(data);
-	// }
+	async function uploadTest() {
+		const pb = new PocketBase('http://127.0.0.1:8090');
+		const data = { name: '박성준', grade: 5, class: 5 };
+		const record = await pb.collection('photo').create(data);
+	}
 </script>
 
-응
-
-<button>업로드</button>
+<div>ㅇㅇㅇ</div>
+<button class="btn btn-primary" on:click={uploadTest}>업로드</button>
